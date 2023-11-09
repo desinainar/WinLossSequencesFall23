74e785ec42e6902e82e9674766034d420f32630d
dnainar <desigamoorthy.nainar@gmail.com>
2023-11-08 19:04:42 -0600

    updated prediction accuracy with graph for adjusted winrate method

--- src/cumulative_scripts.py
+++ src/cumulative_scripts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt 
 
 from utilities import *
 
@@ -20,16 +21,22 @@ team_abbr = {'Kansas City Chiefs' : 'KC', 'Los Angeles Chargers' : 'LAC', 'Denve
 'Atlanta Falcons' : 'ATL', 'New Orleans Saints' : 'NO', 'Carolina Panthers' : 'CAR', 'Tampa Bay Buccaneers' : 'TB',
 'Arizona Cardinals' : 'ARI', 'San Francisco 49ers' : 'SF', 'Seattle Seahawks' : 'SEA', 'Los Angeles Rams' : 'LAR'}
 
+nfl_cumulative = pd.DataFrame(columns=['date', 'season', 'location', 'team1', 'team2', 'team1winrate', 'team2winrate', 'result', 'prediction'])
+epl_cumulative = pd.DataFrame(columns=['date', 'season', 'location', 'team1', 'team2', 'team1winrate', 'team2winrate', 'result', 'prediction'])
+
+
 nfl_df = pd.read_csv('data/processed_data/nfl_live/nfl_current_trimmed.csv')
-data = pd.read_csv('data/raw_data/nfl-2023-raw.csv')
+nfl_raw_data = pd.read_csv('data/raw_data/nfl-2023-raw.csv')
+nfl_teams = nfl_df['team1'].to_list()
+nfl_teams = list(set(nfl_teams))
 
-cumulative = pd.DataFrame(columns=['date', 'season', 'location', 'team1', 'team2', 'team1winrate', 'team2winrate', 'result', 'prediction'])
+epl_df = pd.read_csv('data/processed_data/epl_live/epl_current_trimmed.csv')
+epl_raw_data = pd.read_csv('data/raw_data/epl_current.csv')
+epl_teams = epl_df['team1'].to_list()
+epl_teams = list(set(epl_teams))
 
 
-teams = nfl_df['team1'].to_list()
-teams = list(set(teams))
-
-def weekly_winrate_prediction_accuracy (week_num):
+def weekly_winrate_prediction_accuracy (week_num, data, df, teams):
     next_dates = data.loc[data['Round Number'] == week_num + 1]
     next_dates[['Date', 'Time']] = next_dates['Date'].str.split(' ', expand=True)
     next_dates[['Day', 'Month', 'Year']] = next_dates['Date'].str.split('/', expand=True)
@@ -38,8 +45,8 @@ def weekly_winrate_prediction_accuracy (week_num):
 
     winrates = {}
     for team in teams:
-        nfl_winrate_sequence = get_cumulative_winrate_sequence(nfl_df, 2023, team, next_max_date)
-        curr_winrate = nfl_winrate_sequence['winrate'].iloc[-1]
+        winrate_sequence = get_cumulative_winrate_sequence(df, 2023, team, next_max_date)
+        curr_winrate = winrate_sequence['winrate'].iloc[-1]
         winrates[team] = round(curr_winrate, 3)
 
     curr_dates = data.loc[data['Round Number'] == week_num]
@@ -50,7 +57,7 @@ def weekly_winrate_prediction_accuracy (week_num):
     min_date = min(curr_dates['Date'])
     max_date = max(curr_dates['Date'])
 
-    week = nfl_df.loc[nfl_df['date'] <= max_date]
+    week = df.loc[df['date'] <= max_date]
     week = week.loc[week['date'] >= min_date]
 
     week['team1winrate'] = week['team1']
@@ -64,11 +71,41 @@ def weekly_winrate_prediction_accuracy (week_num):
 
     return week
 
-for i in range(1, 8, 1):
-    week = weekly_winrate_prediction_accuracy(i)
-    cumulative = pd.concat([cumulative, week] , axis=0)
+nfl_correct_prediction = []
+epl_correct_prediction = []
 
-cumulative.to_csv('data/processed_data/nfl_live/nfl_cumulative_testing.csv', index=False)
+for i in range(1, 9, 1):
+    week = weekly_winrate_prediction_accuracy(i, nfl_raw_data, nfl_df, nfl_teams)
+    nfl_correct_prediction.append(sum(week['accuracy']) / len(week['accuracy']))
+    nfl_cumulative = pd.concat([nfl_cumulative, week] , axis=0)
+
+nfl_cumulative.to_csv('data/processed_data/nfl_live/nfl_cumulative_testing.csv', index=False)
+print(nfl_correct_prediction)
+
+for j in range(1, 11, 1):
+    week = weekly_winrate_prediction_accuracy(j, epl_raw_data, epl_df, epl_teams)
+    epl_correct_prediction.append(sum(week['accuracy']) / len(week['accuracy']))
+    epl_cumulative = pd.concat([epl_cumulative, week] , axis=0)
+
+epl_cumulative.to_csv('data/processed_data/epl_live/epl_cumulative_testing.csv', index=False)
+print(epl_correct_prediction)
+
+
+  
+# define data values 
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8])  # X-axis points 
+y = nfl_correct_prediction  # Y-axis points
+
+x2 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+y2 = epl_correct_prediction
+  
+plt.plot(x, y, label = "NFL")
+plt.plot(x2, y2, label = "EPL") 
+plt.xlabel("Week")  # add X-axis label 
+plt.ylabel("Prediction Accuracy")  # add Y-axis label 
+plt.title("Weekly Accuracy")  # add title
+plt.ylim(0, 1) 
+plt.show()
 
 
 
