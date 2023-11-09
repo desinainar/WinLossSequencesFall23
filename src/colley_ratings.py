7a993cc118c7a65c7ee5c8ed8a7bc73ccaef7599
dnainar <desigamoorthy.nainar@gmail.com>
2023-11-09 15:04:29 -0600

    getting colley ratings, need to append to dataframe with game results

--- src/colley_ratings.py
+++ src/colley_ratings.py
@@ -1,6 +1,10 @@
+import penaltyblog as pb
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('data/processed_data/epl_current_trimmed.csv')
-teams = data['Result']
-print(teams)
+nfl_df = pd.read_csv('data/processed_data/nfl_live/nfl_current_trimmed.csv')
+colley = pb.ratings.Colley(nfl_df["score1"], nfl_df["score2"], nfl_df["team1"], nfl_df["team2"])
+ratings = colley.get_ratings() 
+print(ratings)
+
+
